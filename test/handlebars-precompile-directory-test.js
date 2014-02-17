9eec4c9d3a9096aaa1f2fb805c2e4f219c482bae
Joey Baker <joey@byjoeybaker.com>
2014-02-17 13:50:44 -0800

    Test init

---
+++ test/handlebars-precompile-directory-test.js
@@ -0,0 +1,13 @@
+/* global describe, it */
+'use strict';
+
+var chai = require('chai')
+  , should = chai.should()
+  , sinon = require('sinon')
+  , sinonChai = require('sinon-chai')
+
+chai.use(sinonChai)
+
+describe('handlebars-precompile-directory', function(){
+
+})
