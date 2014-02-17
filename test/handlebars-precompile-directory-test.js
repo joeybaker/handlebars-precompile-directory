472c022204345951e2c71d45c2b7fa3ec8544994
Joey Baker <joey@byjoeybaker.com>
2014-02-17 15:03:39 -0800

    Initial

--- test/handlebars-precompile-directory-test.js
+++ test/handlebars-precompile-directory-test.js
@@ -1,13 +1,55 @@
-/* global describe, it */
+/* global describe, it, afterEach */
 'use strict';
 
 var chai = require('chai')
   , should = chai.should()
-  , sinon = require('sinon')
-  , sinonChai = require('sinon-chai')
-
-chai.use(sinonChai)
+  , findit = require('findit')
+  , Handlebars = require('handlebars')
+  , path = require('path')
+  , getTemplates = require('../')
 
 describe('handlebars-precompile-directory', function(){
+  var templatePath = path.join(__dirname, '/fixtures')
 
+  afterEach(function(){
+    Handlebars.partials = {}
+  })
+
+  it('throws if no templates path is given', function(){
+    try {
+      getTemplates({})
+    }
+    catch (e){
+      should.exist(e)
+    }
+  })
+
+  it('ignores files without the extension', function(done){
+    getTemplates({templates: templatePath}, function(templates){
+      templates.should.not.contain.keys('not-a-template')
+      done()
+    })
+  })
+
+  it('registers partials that start with a underscore', function(done){
+    getTemplates({templates: templatePath}, function(templates){
+      templates.should.not.contain.keys('partial')
+      Handlebars.partials.should.contain.keys('partial')
+      done()
+    })
+  })
+
+  it('sets the name to be the relative path of the template', function(done){
+    getTemplates({templates: templatePath}, function(templates){
+      templates.should.contain.keys(['template1', 'template2', 'nested/template1'])
+      done()
+    })
+  })
+
+  it('callsback with an empty object if there are no templates found', function(done){
+    getTemplates({templates: path.join(templatePath, '/templates')}, function(templates){
+      templates.should.be.an.object
+      done()
+    })
+  })
 })
